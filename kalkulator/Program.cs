b404fdc0230f03e2e6e32794769e781733a5d490
chynat <chyla.natalia@gmail.com>
2019-09-28 19:23:04 +0200

    add mul option to menu

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -15,6 +15,7 @@ namespace kalkulator
                 Console.WriteLine("0.Exit");
                 Console.WriteLine("1.Add");
                 Console.WriteLine("2.Diff");
+                Console.WriteLine("3.Mull");
                 int menuOption = Convert.ToInt32(Console.ReadLine());
                 if(menuOption==0)
                 {
